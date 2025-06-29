499a0ce7cec0d88e7de85f3496c2bea4cb408f88
yuriy <yuriy.cherepnev@gmail.com>
2025-06-29 16:39:39 +0300

    edits for all

--- cmd/agent_service.go
+++ cmd/agent_service.go
@@ -5,12 +5,28 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/google/uuid"
+	"github.com/streadway/amqp"
 	"log"
 	"os"
 	"os/exec"
-
-	"github.com/streadway/amqp"
 )
+
+const taskQueueCallbackName = "task_queue_callback"
+const taskQueueName = "task_queue"
+const amqpServerURL = "amqp://admin:admin@localhost:5672/"
+const amqpServerKey = "AMQP_SERVER_URL"
+
+type Code struct {
+	IdUser string `json:"id_user"`
+	IdTask string `json:"id_task"`
+	Code   string `json:"code"`
+}
+
+type SuccessResponse struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+	Queue   string `json:"queue"`
+}
 
 func main() {
 	amqpServerURL := os.Getenv("AMQP_SERVER_URL")
@@ -26,9 +42,23 @@ func main() {
 	defer connectRabbitMQ.Close()
 
 	channelRabbitMQ, err := connectRabbitMQ.Channel()
+
 	if err != nil {
 		panic(err)
 	}
+
+	_, err = channelRabbitMQ.QueueDeclare(
+		taskQueueCallbackName,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		panic(err)
+	}
+
 	defer channelRabbitMQ.Close()
 
 	messages, err := channelRabbitMQ.Consume(
@@ -50,43 +80,48 @@ func main() {
 
 	go func() {
 		for message := range messages {
-			log.Printf(" > Received message: %s\n", message.Body)
-
 			var codeObj Code
 
 			err := json.Unmarshal([]byte(message.Body), &codeObj)
 			if err != nil {
-				log.Fatalf("Ошибка при разборе JSON: %v", err)
+				log.Fatalf("error: %v", err)
+			}
+			uid := uuid.New()
+			fileName := uid.String() + ".py"
+			dirName := "code"
+			makeFile(dirName, fileName, codeObj.Code)
+			runCode(dirName, fileName)
+
+			response := SuccessResponse{
+				Success: true,
+				Message: "Code successfully run",
+				Queue:   taskQueueCallbackName,
+			}
+			jsonBody, err := json.Marshal(response)
+
+			fmt.Println(jsonBody)
+			message := amqp.Publishing{
+				ContentType: "application/json",
+				Body:        jsonBody,
 			}
 
-			uid := uuid.New()
-			dirName := uid.String()
-			makeDir(dirName)
-			makeFile(dirName, codeObj.Code, "main.py")
-			//TODO написать тест для кода
-			//mkfile(dirName, code_obj.Test, "test_hello.py")
-			//run(dirName)
-			fmt.Println(codeObj.Code)
-
+			err = channelRabbitMQ.Publish(
+				"",
+				taskQueueCallbackName,
+				false,
+				false,
+				message,
+			)
 		}
 	}()
 
 	<-forever
 }
 
-func makeDir(dirName string) {
-	permissions := os.ModeDir | 0755
-	err := os.Mkdir(dirName, permissions)
-	if err != nil {
-		log.Printf("Ошибка при создании директории: %v", err)
-	}
-	fmt.Printf("Директория '%s' успешно создана.\n", dirName)
-}
-
-func makeFile(dirName string, base64String string, fileName string) {
+func makeFile(dirName string, fileName string, base64String string) {
 	decodedBytes, er := base64.StdEncoding.DecodeString(base64String)
 	if er != nil {
-		log.Fatalf("Ошибка при декодировании Base64: %v", er)
+		log.Fatalf("makeFile error: %v", er)
 	}
 	fileContent := string(decodedBytes)
 	fullName := "./" + dirName + "/" + fileName
@@ -94,24 +129,26 @@ func makeFile(dirName string, base64String string, fileName string) {
 	data := []byte(fileContent)
 	err := os.WriteFile(fullName, data, os.FileMode(permissions))
 	if err != nil {
-		log.Fatalf("Ошибка при создании файла: %v", err)
+		return
 	}
-	fmt.Printf("Файл '%s' успешно создан с содержимым.\n", fileName)
 }
 
-func run(taskDir string) {
+func runCode(dirName string, fileName string) (bool, string) {
 	currentDir, err := os.Getwd()
 	if err != nil {
 		log.Fatalf("Ошибка при получении текущей директории: %v", err)
 	}
-	dir := currentDir + "/" + taskDir
+	dir := currentDir + "/" + dirName
 	err = os.Chdir(dir)
 	if err != nil {
 		log.Fatalf("Ошибка при изменении директории: %v", err)
 	}
-	fileName := "test_hello.py"
-	cmd := exec.Command("python", "-m", "unittest", fileName)
+	//fileName := "test_hello.py"
+	//cmd := exec.Command("python", "-m", "unittest", fileName)
+
+	cmd := exec.Command("python3", fileName)
 	output, err := cmd.CombinedOutput()
+
 	if err != nil {
 		log.Fatalf("Ошибка при выполнении Python кода: %v\nВывод: %s", err, string(output))
 	}
@@ -119,6 +156,7 @@ func run(taskDir string) {
 	// Вывод результата
 	// todo: записать в топик RMQ id_user_id_UUID
 	// {  data: output}
-	fmt.Printf("Вывод Python:\n%s\n", string(output))
+
+	return true, string(output)
 
 }
--- cmd/push_service.go
+++ cmd/push_service.go
@@ -3,136 +3,104 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/websocket"
+	"github.com/streadway/amqp"
 	"log"
 	"net/http"
-
 	"os"
-	"github.com/streadway/amqp"
-	"github.com/gorilla/websocket"
 )
 
-
+const taskQueueCallbackName = "task_queue_callback"
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
-		return true // Allow all origins (for development).  In production, specify allowed origins.
+		return true
 	},
 }
 
 func handleConnections(w http.ResponseWriter, r *http.Request) {
-	
-	type Code struct {
-		IDTask   string `json:"id_task"`
-		TpRunner string `json:"tp_runner"`
-		Code     string `json:"code"`
-		Test     string `json:"test"`
+	type CallbackMessage struct {
+		Success bool   `json:"success"`
+		Message string `json:"message"`
+		Queue   string `json:"queue"`
 	}
 
-	// Define RabbitMQ server URL.
+	ws, _ := upgrader.Upgrade(w, r, nil)
+
 	amqpServerURL := os.Getenv("AMQP_SERVER_URL")
 	if amqpServerURL == "" {
-		amqpServerURL = "amqp://guest:guest@localhost:5672/" // Default URL if not set
+		amqpServerURL = "amqp://admin:admin@localhost:5672/"
 	}
 
-	// Create a new RabbitMQ connection.
 	connectRabbitMQ, err := amqp.Dial(amqpServerURL)
 	if err != nil {
 		panic(err)
 	}
 	defer connectRabbitMQ.Close()
 
-	// Opening a channel to our RabbitMQ instance over
-	// the connection we have already established.
 	channelRabbitMQ, err := connectRabbitMQ.Channel()
 	if err != nil {
 		panic(err)
 	}
 	defer channelRabbitMQ.Close()
-	
 
-    // todo: заменен QueueService1 на динамический
-	// id_user_id_task 
-    // Subscribing to QueueService1 for getting messages.
 	messages, err := channelRabbitMQ.Consume(
-		"QueueService1", // queue name
-		"",              // consumer
-		true,            // auto-ack
-		false,           // exclusive
-		false,           // no local
-		false,           // no wait
-		nil,             // arguments
+		taskQueueCallbackName,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
 	)
 	if err != nil {
 		log.Println(err)
 	}
 
-	// Build a welcome message.
 	log.Println("Successfully connected to RabbitMQ")
 	log.Println("Waiting for messages")
 
-     	// Upgrade initial GET request to a websocket
-	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Make sure we close the connection when the function returns
+
 	defer ws.Close()
 
-
-	// Make a channel to receive messages into infinite loop.
 	forever := make(chan bool)
 
 	go func() {
 		for message := range messages {
-			// For example, show received message in a console.
 			log.Printf(" > Received message: %s\n", message.Body)
-			// Создание экземпляра структуры Task
+			var messageObj CallbackMessage
 
-			var code_obj Code
-
-			// Разбор JSON строки в структуру
-			err := json.Unmarshal([]byte(message.Body), &code_obj)
+			err := json.Unmarshal([]byte(message.Body), &messageObj)
 			if err != nil {
 				log.Fatalf("Ошибка при разборе JSON: %v", err)
 			}
-			// Write message back to browser
-			err = ws.WriteMessage(websocket.TextMessage, []byte(code_obj.Code))
+			err = ws.WriteMessage(websocket.TextMessage, []byte(messageObj.Message))
 			if err != nil {
 				log.Println(err)
 				return
 			}
-				fmt.Println(code_obj.Code)
-
 		}
 	}()
 
 	<-forever
 
-
-	//  for {
-	// 	// Read message from browser
-	// 	_, msg, err := ws.ReadMessage()
-	// 	if err != nil {
-	// 		log.Println(err)
-	// 		return
-	// 	}
-
-	// 	// Print the message to the console
-	// 	fmt.Printf("Received: %s\n", msg)
-
-	// 	// Write message back to browser
-	// 	err = ws.WriteMessage(websocket.TextMessage, msg)
-	// 	if err != nil {
-	// 		log.Println(err)
-	// 		return
-	// 	}
-	// }
+	for {
+		_, msg, err := ws.ReadMessage()
+		if err != nil {
+			return
+		}
+		err = ws.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			return
+		}
+	}
 }
 
 func main() {
-	
 	fmt.Println("Starting WebSocket server...")
 	http.HandleFunc("/ws", handleConnections)
-
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Fatal(http.ListenAndServe(":8082", nil))
 }
